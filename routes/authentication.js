0033dd915781c527d3116e21ce9d7f7f3c25bcd2
Filipe Freire <filipefreire@protonmail.com>
2020-07-31 11:42:49 +0100

    Complete iterations 1 to 3

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,46 @@
 'use strict';
 
 const { Router } = require('express');
-const authenticationRouter = Router();
+const authRouter = Router();
 
-module.exports = authenticationRouter;
+const User = require('../models/user');
+
+const routeGuard = require('../middleware/route-guard');
+const bcryptjs = require('bcryptjs');
+const passport = require('passport');
+const bcryptSalt = 10;
+
+authRouter.get('/sign-up', (req, res) => {
+  res.render('authentication/sign-up');
+});
+
+authRouter.post(
+  '/sign-up',
+  passport.authenticate('sign-up', {
+    successRedirect: '/authentication/private',
+    failureRedirect: '/authentication/sign-up'
+  })
+);
+
+authRouter.get('/sign-in', (req, res, next) => {
+  res.render('authentication/sign-in');
+});
+
+authRouter.post(
+  '/sign-in',
+  passport.authenticate('sign-in', {
+    successRedirect: '/',
+    failureRedirect: '/authentication/sign-in'
+  })
+);
+
+authRouter.post('/sign-out', (req, res, next) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+authRouter.get('/private', routeGuard, (req, res, next) => {
+  res.render('authentication/private');
+});
+
+module.exports = authRouter;
